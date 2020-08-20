4a6d86a4fcf105139d03cdb9294470ed3394ba05
galdinocgm <gcgm@cin.ufpe.br>
2020-08-19 23:37:37 -0300

    Added food state update to the FoodSpecifics component

--- src/components/FoodSpecifics/index.js
+++ src/components/FoodSpecifics/index.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const FoodSpecifics = () => {
+import api from '../../services/api.js'
+
+const FoodSpecifics = (props) => {
+
+    const id = parseInt(props.match.params.number);
+
+    const [food, setFood] = useState({});
+
+    // Using the useEffect hook to update the food state after the component mounts.
+    useEffect(() => {
+
+        function getFoodById(foods){
+            for(let food of foods){
+                if(food.id === id){
+                    return food;
+                }
+            }
+        }
+
+        async function fetchFood(){
+            let foods = await api.get('/foods/').then(res => res.data);
+            return getFoodById(foods);
+        }
+
+        async function updateFoodState(){
+            let currentFood = await fetchFood();
+            setFood(currentFood);
+        }
+
+        updateFoodState();
+    } , []);
+
     return(
-        <h1>Hello World</h1>
+        <>           
+            <h1>Hello World</h1>
+        </>
     );
 }
 
 export default FoodSpecifics;
+
